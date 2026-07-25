5c26fe5a19132361169c7facbaf149e9156d3539
Vridhi <rajeevvridhi@gmail.com>
2026-07-25 18:11:03 +0530

    Delete backend directory

--- backend/server.js
+++
@@ -1,6 +0,0 @@
-require("dotenv").config();
-const app = require("./src/app");
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server started on port: ${PORT}`);
-});
--- backend/src/routes/ai.routes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express'); 
-const router = express.Router(); 
-const aiController = require('../controllers/ai.controller')
-router.post('/get-review' , aiController.getReview );
-
-module.exports = router ; 
