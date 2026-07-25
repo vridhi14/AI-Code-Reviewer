63c5af80d5f97395d02b2ce177ec64188bd0821c
Vridhi <rajeevvridhi@gmail.com>
2026-07-25 13:41:27 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,11 @@ const app = express();
 const aiRoutes = require('./routes/ai.routes')
 const cors = require('cors');
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
+
 app.get("/", (req, res) => {
     res.send("running bro!!");
 });
